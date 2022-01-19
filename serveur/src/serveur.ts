5592529b8ca72488384029d2dd330909f0c654ef
olas7 <73588237+olas7@users.noreply.github.com>
2022-01-20 00:12:52 +0100

    modification filtres

--- serveur/src/serveur.ts
+++ serveur/src/serveur.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
 import elasticsearch from "elasticsearch";
+const bodyParser = require("body-parser");
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
 
 const client = new elasticsearch.Client({
     host: "localhost:9200",
@@ -12,14 +14,14 @@ const client = new elasticsearch.Client({
 });
 
 app.get('/api/jeux/all', (req, res) => {
-    
-    let query = async function() {
+
+    let query = async function () {
         const body = await client.search({
             index: "steam-database",
             size: 50,
             body: {
                 query: {
-                    match_all: {  }
+                    match_all: {}
                 },
                 sort: [{
                     appid: {
@@ -27,14 +29,14 @@ app.get('/api/jeux/all', (req, res) => {
                     }
                 }]
             },
-           
+
         });
 
         let rep: any[] = body.hits.hits;
 
         rep = rep.map((value) => {
             // console.log(value);
-            
+
             return ({
                 appid: value._source.appid,
                 name: value._source.name,
@@ -52,8 +54,8 @@ app.get('/api/jeux/all', (req, res) => {
 });
 
 app.get('/api/jeux/:appid', (req, res) => {
-    
-    let query = async function() {
+
+    let query = async function () {
         let body = await client.search({
             index: "steam-database",
             body: {
@@ -69,9 +71,9 @@ app.get('/api/jeux/:appid', (req, res) => {
 
         if (body.hits.hits.length > 0) {
 
-            rep = rep.map((value) => {      
-                
-                          
+            rep = rep.map((value) => {
+
+
                 return ({
                     appid: value._source.appid,
                     name: value._source.name,
@@ -82,7 +84,7 @@ app.get('/api/jeux/:appid', (req, res) => {
                     detailed_description: value._source.detailed_description,
                     // about_the_game: value._source.about_the_game,
                     screenshots: JSON.parse(value._source.screenshots.replace(/'/g, "\"")),
-                    mac_requirements:  value._source.mac_requirements,
+                    mac_requirements: value._source.mac_requirements,
                     pc_requirements: value._source.pc_requirements,
                     minimum: value._source.minimum,
                     short_description: value._source.short_description,
@@ -90,14 +92,14 @@ app.get('/api/jeux/:appid', (req, res) => {
                     publisher: value._source.publisher,
                     positive_ratings: value._source.positive_ratings,
                     negative_ratings: value._source.negative_ratings,
-                    linux_requirements: value._source.linux_requirements, 
+                    linux_requirements: value._source.linux_requirements,
                     movies: value._source.linux_requirements
                 });
             });
         }
 
-       
-        
+
+
 
         res.send(rep);
     }
@@ -105,31 +107,52 @@ app.get('/api/jeux/:appid', (req, res) => {
     query();
 });
 
+
+interface IQuery {
+    page: string;
+    name: string;
+    release_date: string;
+    developer: string;
+    publisher: string;
+    platforms: string;
+    required_age: string;
+    categories: string;
+    genres: string;
+    users_tags: string;
+    positive_reviews: string;
+
+}
+
 app.get("/api/recherche", (req, res) => {
-  
+
+    console.log(req.body);
     
-    let page: any = req.query.page;   
-    
+
+    let page: any = req.query.page;
+
     let q: any = req.query;
 
-    
-     
     let vide = 1;
-    
+
     for (let key in req.query) {
         if (req.query[key] == "null") {
             req.query[key] = "";
         }
-        
+
         if (key != "page" && req.query[key] != "") {
             vide = 0;
         }
+
+        if (key == "positive_reviews") {
+            // @ts-ignore
+            // req.query[key] = 0;
+        }
     }
 
-    
+
 
     if (req.query.release_date === "" || req.query.release_date === undefined || req.query.release_date === "null") {
-        req.query.release_date = "2000-01-01"
+        req.query.release_date = undefined;
     }
 
     if (isNaN(page)) {
@@ -144,16 +167,19 @@ app.get("/api/recherche", (req, res) => {
         page = 1;
     }
 
-    console.log(req.query);
+
 
     console.log("page " + page);
-    
+
+    req.query.platforms = (<any>req.query).platforms.replace(" ", ";");
+
+    console.log(req.query);
 
     let name = req.query.name;
-   
-    let query = async function() {
+
+    let query = async function () {
         let body;
-        
+
         if (vide) {
             body = await client.search({
                 index: "steam-database",
@@ -169,10 +195,10 @@ app.get("/api/recherche", (req, res) => {
                         }
                     }]
                 },
-               
+
             });
 
-        } else { 
+        } else {
             body = await client.search({
                 index: "steam-database",
                 size: 50,
@@ -181,45 +207,121 @@ app.get("/api/recherche", (req, res) => {
                     query: {
                         bool: {
                             must: [
-                                
-                                
+                                {
+                                    match: {
+                                        name: {
+                                            query: req.query.name,
+                                            operator: "and",
+                                            zero_terms_query: "all"
+                                        }
+                                    }
+                                },
+                                // { 
+                                //     match: { 
+                                //         release_date: {
+                                //             query: req.query.release_date,
+                                //             operator: "and",
+                                //             zero_terms_query: "all"
+                                //         }
+                                //     } 
+                                // },
+                                { 
+                                    match: { 
+                                        developer: {
+                                            query: req.query.developer,
+                                            operator: "and",
+                                            zero_terms_query: "all"
+                                        }
+                                    } 
+                                },
+                                { 
+                                    match: { 
+                                        publisher: {
+                                            query: req.query.publisher,
+                                            operator: "and",
+                                            zero_terms_query: "all"
+                                        }
+                                    } 
+                                },
+                                { 
+                                    match: { 
+                                        platforms: {
+                                            query: req.query.platforms,
+                                            operator: "and",
+                                            zero_terms_query: "all"
+                                        }
+                                    } 
+                                },
+                                { 
+                                    match: { 
+                                        required_age: {
+                                            query: req.query.required_age,
+                                            operator: "and",
+                                            zero_terms_query: "all"
+                                        }
+                                    } 
+                                },
+                                { 
+                                    match: { 
+                                        categories: {
+                                            query: req.query.categories,
+                                            operator: "and",
+                                            zero_terms_query: "all"
+                                        }
+                                    } 
+                                },
+                                { 
+                                    match: { 
+                                        genres: {
+                                            query: req.query.genres,
+                                            operator: "and",
+                                            zero_terms_query: "all"
+                                        }
+                                    } 
+                                },
+                                { 
+                                    match: { 
+                                        steamspy_tags: {
+                                            query: req.query.users_tags,
+                                            operator: "and",
+                                            zero_terms_query: "all"
+                                        }
+                                    } 
+                                },
+                                // { 
+                                //     match: { 
+                                //         positive_ratings: {
+                                //             query: req.query.positive_reviews,
+                                //             operator: "and",
+                                //             zero_terms_query: "all"
+                                //         }
+                                //     } 
+                                // },
                             ],
-                            // certaines clées sont pas bonnes
-                            should: [
-                                {match: {name: req.query.name}},
-                                {match: {release_date: req.query.release_date}},
-                                {match: {developer: req.query.developer}},
-                                {match: {publisher: req.query.publisher}},
-                                {match: {platforms: req.query.platforms}},
-                                {match: {required_age: req.query.required_age}},
-                                {match: {categories: req.query.categories}},
-                                {match: {genres: req.query.genres}},
-                                {match: {users_tags: req.query.users_tags}},
-                                {match: {positive_reviews: req.query.positive_reviews}},
-                            ]
                         }
                     },
                     sort: [{
-                         appid: {
-                             order: "desc"
-                         }
-                    }],          
+                        // appid: {
+                        //      order: "desc"
+                        // }
+                    }],
                 },
             });
         }
-        
+
 
         let rep: any[] = body.hits.hits;
+
+        
 
         if (body.hits.hits.length > 0) {
 
             rep = rep.map((value) => {
-                // console.log(value);
-                
+
                 return ({
                     appid: value._source.appid,
                     name: value._source.name,
-                    release_date: value._source.release_date,
+                    release_date: "a",
                     positive_ratings: value._source.positive_ratings,
                     background: value._source.background,
                     header_image: value._source.header_image
@@ -230,29 +332,30 @@ app.get("/api/recherche", (req, res) => {
         res.send(rep);
     }
 
-    query().catch(() => {
-        console.log("a");
-        res.send({erreur: "erreur"});
-        
+    query().catch((err) => {
+        console.log(err);
+        res.send({ erreur: "erreur" });
+
     });
-    
+
 });
 
 app.get("/api/fuzzysearch/", (req, res) => {
     let name = req.query.name;
 
-    let query = async function() {
+    let query = async function () {
         let body;
-        
+
         body = await client.search({
             index: "steam-database",
             size: 5,
             body: {
                 query: {
-                    fuzzy: {
+                    match: {
                         name: {
-                            value: name,
+                            query: name,
                             fuzziness: "AUTO"
+                            
                         }
                     }
                 },
@@ -260,17 +363,17 @@ app.get("/api/fuzzysearch/", (req, res) => {
                     // appid: {
                     //     order: "asc"
                     // }
-                }],          
+                }],
             },
         });
-        
+
         let rep: any[] = body.hits.hits;
 
         if (body.hits.hits.length > 0) {
 
             rep = rep.map((value) => {
                 // console.log(value);
-                
+
                 return ({
                     // appid: value._source.appid,
                     name: value._source.name,
@@ -287,7 +390,7 @@ app.get("/api/fuzzysearch/", (req, res) => {
 
     query();
 });
-  
+
 app.listen(3001, () => {
     console.log(`écoute sur http://localhost:${3000}`)
 });
